ffc6cb2e918378e6f75512a33eb5756c4b791a0a
Ashfaq Mahmood Rafi <ashfaq.bd92@gmail.com>
2023-08-20 20:14:51 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hola Amigo, Como Estas, Good?</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
