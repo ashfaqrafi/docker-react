c14192d5f80a555858d760527cfc38a5a3fd6d11
ashfaqrafi <ashfaq.bd92@gmail.com>
2023-10-07 19:57:22 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola Amigo, Como Estas, Good?</p>
+        <p>Hola Amigo, Como Estas?</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
